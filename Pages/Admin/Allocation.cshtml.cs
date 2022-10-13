d4dd4b173b6846e163b2ad1780de87a74cdde65b
Noel Mthembu <ST10128116@rcconnect.edu.za>
2022-10-13 14:04:09 +0200

    Fixed Allocations button

--- Pages/Admin/Allocation.cshtml.cs
+++ Pages/Admin/Allocation.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -23,12 +24,99 @@ namespace APPROG7311.Pages.Admin
         public String successMessage = "";
         public void OnGet()
         {
+            try
+            {
+                String connectionString = "Server=tcp:disaster-alleviation-foundation.database.windows.net,1433;Initial Catalog=Disaster_Alleviation_Foundation_DB;Persist Security Info=False;User ID=dafadmin;Password=P@ssw0rd;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql_disaster = "SELECT * FROM DISASTER";
+                    String sql_monetray = "SELECT * FROM MONETRAY";
+                    String sql_goods = "SELECT * FROM GOODS";
+                    using (SqlCommand command = new SqlCommand(sql_disaster, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                disasterInfo.DISASTER_NAME = reader.GetString(2);
+                                listdis.Add(disasterInfo);
+                            }
+                        }
+                    }
+                    //
+                    using (SqlCommand command = new SqlCommand(sql_monetray, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                monetrayInfo.AMOUNT = reader.GetDouble(2);
+                                listmon.Add(monetrayInfo);
+                            }
+                        }
+                    }
+                    //
+                    using (SqlCommand command = new SqlCommand(sql_goods, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                goodsInfo.CATEGORY = reader.GetString(4);
+                                goodsInfo.NEW_CATEGORY = reader.GetString(5);
+                                listgoo.Add(goodsInfo);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Expection: " + ex.ToString());
+            }
+        }
+        public void OnPost() 
+        {
+            paymentInfo.ALL_AMOUNT = Convert.ToDouble(Request.Form["ALL_AMOUNT"]);
+            paymentInfo.ACTIVE_DISASTER = Request.Form["ACTIVE_DISASTER"];
+            paymentInfo.ALL_GOODS = Request.Form["ALL_GOODS"];
+
+
+            try
+            {
+                String connectionString = "Data Source=disaster-alleviation-foundation.database.windows.net;Initial Catalog=Disaster_Alleviation_Foundation_DB;User ID=dafadmin;Password=P@ssw0rd;Connect Timeout=60;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "INSERT INTO ALLOCATIONS VALUES(@ALL_GOODS, @ALL_AMOUNT, @ACTIVE_DISASTER)";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@ALL_GOODS", monetrayInfo.IS_CHECKED);
+                        command.Parameters.AddWithValue("@ALL_AMOUNT", monetrayInfo.AMOUNT);
+                        command.Parameters.AddWithValue("@ACTIVE_DISASTER", monetrayInfo.START_DATE);
+                        command.ExecuteNonQuery();
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+                return;
+            }
+            paymentInfo.ALL_AMOUNT = 0;
+            paymentInfo.ACTIVE_DISASTER = "";
+            paymentInfo.ALL_GOODS = "";
+
+            successMessage = "Added Successfully!";
+
         }
         public class AllocationInfo
         {
             public String ID;
-            public String PAYER_NAME;
-            public Double PAYMENT;
+            public String ALL_GOODS;
+            public Double ALL_AMOUNT;
             public String ACTIVE_DISASTER;
 
 
